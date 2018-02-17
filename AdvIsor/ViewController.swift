82322a85decb1ed3324ecc29ea66e2d3638d73fe
Daniel Kweon <danielkweon@daniels-macbook-pro-2.local>
2018-02-17 14:36:16 -0500

    Initial Commit

---
+++ AdvIsor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdvIsor
+//
+//  Created by Daniel Kweon on 2/17/18.
+//  Copyright © 2018 Daniel Kweon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
